918ef1eb77d38775353b0fbae41f6ab6a71ef225
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-05-13 20:05:00 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-blue-100 min-h-screen flex items-center justify-center'>
+      <div className='text-center'>
+        <h1 className='text-2xl font-bold mb-4'>Welcome to the App</h1>
+        <p className='text-green-700'>This is a simple React application.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
